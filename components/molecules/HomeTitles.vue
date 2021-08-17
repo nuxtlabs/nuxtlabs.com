75ad8dfe4c788532ef85209291b859edb32154b1
Florent <flo@nuxtjs.com>
2021-08-17 15:05:46 +0200

    :sparkles: (HomeTitles) create home titles component

---
+++ components/molecules/HomeTitles.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="flex-1 flex flex-col items-end justify-center space-y-4">
+    <li v-for="(title, index) in titles" :key="index">
+      <h1 class="text-display-1 font-inter italic title-style font-bold uppercase">{{ title }}</h1>
+    </li>
+  </ul>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    titles: {
+      type: Array,
+      default: () => [
+        'NuxtJS',
+        'Docus',
+        'Vue Telescope'
+      ]
+    }
+  },
+  setup() {
+
+  },
+})
+</script>
+<style lang="postcss">
+.title-style {
+  text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
+  color: black;
+}
+</style>
