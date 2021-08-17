f6b9a47edda71a8721697cb2f2309c8fc2813f9f
Florent <flo@nuxtjs.com>
2021-08-17 15:04:31 +0200

    :sparkles: (AppLayout) override docus AppLayout - update css classes

---
+++ components/organisms/app/AppLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="w-full h-screen">
+    <slot v-if="layout.header" name="header">
+      <AppHeader :aside="layout.aside" />
+    </slot>
+
+    <div class="lg:flex h-full" :class="layout.fluid ? '' : 'd-container'">
+      <slot v-if="layout.aside" name="aside">
+        <AppAside :class="layout.asideClass" />
+      </slot>
+
+      <!-- TODO: possibility to unwrap div ? -->
+      <div class="flex-auto w-full min-w-0 lg:static lg:max-h-screen lg:overflow-visible mt-8 fix-wrapper">
+        <slot />
+      </div>
+    </div>
+
+    <slot v-if="layout.footer" name="footer"><AppFooter /></slot>
+
+    <PoweredByDocus v-if="$docus.settings.credits" />
+  </div>
+</template>
+
+<script>
+import { computed, defineComponent, useContext } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const { $docus } = useContext()
+
+    const layout = computed(() => $docus.layout.value)
+
+    return {
+      layout
+    }
+  }
+})
+</script>
+<style lang="postcss">
+.fix-wrapper > div {
+  @apply h-full p-6 xl:p-8 2xl:p-16;
+  padding-top: calc(var(--header-height));
+}
+</style>
