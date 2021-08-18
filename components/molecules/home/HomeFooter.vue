4cb18aad8e650e3c692f184005524b2ba8e6c8f2
Florent <flo@nuxtjs.com>
2021-08-18 16:07:45 +0200

    :recycle: (home) update

--- components/molecules/home/HomeFooter.vue
+++ components/molecules/home/HomeFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="w-full flex flex-col space-y-1">
     <div class="relative z-10">{{ location }}</div>
-    <div class="font-bold relative z-10">{{ email }}</div>
+    <div class="font-bold relative z-10">hello@nuxtlabs.com</div>
     <ul class="flex space-x-2">
       <li v-for="link in links" :key="link.name">
-        <Link :id="link.gradientName" :to="link.href" :alt="link.name" blank class="relative z-10 gradient">{{ link.name }}</Link>
+        <Link :to="link.href" :alt="link.name" blank class="relative z-10 gradient">{{ link.name }}</Link>
         <div class="absolute opacity-60 w-128 h-128 z-0 pointer-events-none -bottom-full -left-full">
-          <img :src="`/img/socialGradients/${link.gradientName}.svg`" class="relative" />
+          <img :src="`/img/socialGradients/${link.gradientImg}.svg`" class="relative" />
         </div>
       </li>
     </ul>
@@ -20,10 +20,6 @@ export default defineComponent({
     location: {
       type: String,
       default: 'Bordeaux, France'
-    },
-    email: {
-      type: String,
-      default: 'hello@nuxtlabs.com'
     }
   },
   setup() {
@@ -31,22 +27,22 @@ export default defineComponent({
       {
         name: 'LinkedIn',
         href: 'https://www.linkedin.com/company/nuxtlabs/mycompany/',
-        gradientName: 'linkedin'
+        gradientImg: 'linkedin'
       },
       {
         name: 'Github',
         href: 'https://github.com/nuxt',
-        gradientName: 'github'
+        gradientImg: 'github'
       },
       {
         name: 'Twitter',
         href: 'https://twitter.com/nuxt_js',
-        gradientName: 'twitter'
+        gradientImg: 'twitter'
       },
       {
         name: 'Discord',
         href: 'https://discord.com/invite/ps2h6QT',
-        gradientName: 'discord'
+        gradientImg: 'discord'
       }
     ]
 
