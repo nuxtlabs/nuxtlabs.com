eccd4930a4fe84a7c507483d25bbe4666464e876
Florent <flo@nuxtjs.com>
2021-08-23 14:23:31 +0200

    :dizzy: (HomeTitles) add reverse anim and opacity on hover

--- components/molecules/home/HomeTitles.vue
+++ components/molecules/home/HomeTitles.vue
@@ -2,8 +2,8 @@
   <ul class="flex-1 flex flex-col items-end justify-center space-y-4">
     <li v-for="({ title, component }, index) in titles" :key="index">
       <div class="flex flex-row-reverse items-center">
-        <Link :class="component" to="#">
-          <h1 class="lg:text-display-3 xl:text-display-2 2xl:text-display-1 title relative z-10 title-shadow text-right">{{ title }}</h1>
+        <Link :class="component" to="#" @mouseover.native="hover = true" @mouseleave.native="hover = !hover">
+          <h1 class="lg:text-display-3 xl:text-display-2 2xl:text-display-1 title relative z-10 title-shadow text-right" :class="{ 'opacity-30': hover }" >{{ title }}</h1>
         </Link>
         <hr id="line" class="relative border-none h-0.5 rounded-lg bg-white text-white w-4 ml-2 2xl:ml-4 mr-6 xl:mr-8 2xl:mr-12 opacity-0" />
         <p class="text-lg xl:text-xl 2xl:text-2xl font-sans lg:max-w-96 xl:w-max-128 font-normal opacity-0">
@@ -29,6 +29,7 @@ export default defineComponent({
     }
   },
   setup() {
+    const hover = ref(false)
     const titles = ref([
       { title: 'NuxtJS', component: 'NuxtAnimations' },
       { title: 'Docus', component: 'DocusAnimations' },
@@ -36,7 +37,8 @@ export default defineComponent({
     ])
 
     return {
-      titles
+      titles,
+      hover
     }
   }
 })
@@ -47,28 +49,53 @@ export default defineComponent({
 .VueTelescopeAnimations {
   &:hover {
     > h1 {
-      animation: colorText 2s forwards
+      animation: colorTextIn 1s forwards;
+      opacity: 1;
     }
     ~ p {
-      animation: subTitle 2s forwards
+      animation: subTitleIn 1s forwards
     }
     ~ hr {
-      animation: line 1s forwards
+      animation: lineIn 1s forwards
     }
   }
 }
 
-@keyframes colorText {
+.NuxtAnimations,
+.DocusAnimations,
+.VueTelescopeAnimations {
+  > h1 {
+    animation: colorTextOut 1s forwards;
+    opacity: 1;
+  }
+  ~ p {
+    animation: subTitleOut 1s forwards
+  }
+  ~ hr {
+    animation: lineOut 1s forwards
+  }
+}
+
+@keyframes colorTextIn {
   from { color: black; }
   to { color: white; }
 }
 
-@-webkit-keyframes subTitle {
+@keyframes colorTextOut {
+  from { color: white; }
+  to { color: black; }
+}
+
+@-webkit-keyframes subTitleIn {
   from { opacity: 0; }
   to { opacity: 1; }
 }
+@-webkit-keyframes subTitleOut {
+  from { opacity: 1; }
+  to { opacity: 0; }
+}
 
-@-webkit-keyframes line {
+@-webkit-keyframes lineIn {
   from {
     opacity: 0;
   }
@@ -80,6 +107,21 @@ export default defineComponent({
   }
   100% {
     @apply w-12 2xl:w-16
+  }
+}
+
+@-webkit-keyframes lineOut {
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+  0% {
+    @apply w-12 2xl:w-16
+  }
+  100% {
+    @apply w-2 2xl:w-4
   }
 }
 </style>
