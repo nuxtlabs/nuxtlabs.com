b0c080626d35077a71e31f619905456f55a2b0d8
Florent <flo@nuxtjs.com>
2021-08-23 09:51:26 +0200

    :sparkles: (DocusAnimations) add animations for docus title

---
+++ components/molecules/home/DocusAnimations.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <img id="ghost" :src="`img/docus/ghost.svg`" class="absolute -bottom-full right-24 transform" />
+    <img id="shape" :src="`img/docus/shape.svg`" class="absolute bottom-38 left-0 opacity-0 transform" />
+  </div>
+</template>
+<style lang="postcss" scoped>
+.DocusAnimations {
+  &:hover {
+    ~ div {
+      > #shape {
+        animation: fadeInShape 3s forwards;
+      }
+      > #ghost {
+        animation: ghostAnim 1s forwards cubic-bezier(0.83, 0, 0.17, 1);
+      }
+    }
+  }
+}
+
+@keyframes fadeInShape {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
+@-webkit-keyframes ghostAnim {
+  0% {
+    @apply -bottom-full;
+  }
+  100% {
+    @apply -bottom-20 z-20;
+  }
+}
+
+</style>
