d33c0a3a4b32c1e85542a9683919393c25daa023
Florent <flo@nuxtjs.com>
2021-08-23 09:52:36 +0200

    :bento: (BarLeft/Bottom/Mid) add bar vue telescope svg component

---
+++ components/atoms/vuetelescope/BarBottom.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg class="barBottom" width="316" height="339" viewBox="0 0 316 339" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0)">
+    <line x1="-190.685" y1="630.591" x2="540.315" y2="-293.409" stroke="#4DDFB5" stroke-width="40"/>
+    </g>
+    <defs>
+    <clipPath id="clip0">
+    <rect width="316" height="339" fill="white"/>
+    </clipPath>
+    </defs>
+  </svg>
+</template>
