0eb564110be56f8250c7474493d81626ca47fa01
Florent <flo@nuxtjs.com>
2021-08-25 11:31:56 +0200

    :sparkles: (AppHeader) update for responsive

--- components/organisms/app/AppHeader.vue
+++ components/organisms/app/AppHeader.vue
@@ -1,16 +1,18 @@
 <template>
   <header class="absolute w-full top-0 z-50">
     <div class="d-header xl:p-4 2xl:p-8">
-      <div class="flex h-full flex-none mx-auto px-1 sm:px-3 lg:px-6">
+      <div class="flex flex-row-reverse lg:flex-row h-full flex-none mx-auto px-1 sm:px-3 lg:px-6">
         <NavigationButton v-if="aside" class="px-2.5" />
-
         <Link
-          :class="[aside ? 'justify-center lg:justify-start' : 'justify-start']"
           class="flex items-center flex-1 lg:flex-none lg:w-60"
           :to="localePath('/')"
         >
           <!-- "mr-4 lg:mr-0" to optically center logo text -->
-          <Logo :settings="settings" class="h-5 w-4/5 sm:h-6 md:h-7 mr-4 lg:mr-0" />
+          <Logo :setings="settings" logoPath="/img/Logo.png"
+            class="hidden lg:block h-5 w-4/5 sm:h-6 md:h-7 mr-4 lg:mr-0" />
+            <!-- Mobile -->
+          <Logo :setings="settings" logoPath="icon.png"
+            class="lg:hidden h-10 w-14 sm:h-14 sm:w-18 md:h-20 md:w-24 lg:w-4/5 lg:h-8" />
         </Link>
 
         <div class="items-center hidden lg:flex lg:flex-1">
