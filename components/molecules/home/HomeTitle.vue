12f4dbdebac951a3fd6ddac9b1331fd277fa16ec
Florent <flo@nuxtjs.com>
2021-08-30 10:56:48 +0200

    :recycle: (HomeTitle) wip - refacto

--- components/molecules/home/HomeTitle.vue
+++ components/molecules/home/HomeTitle.vue
@@ -1,18 +1,18 @@
 <template>
-  <li class="block" :class="[{ 'block': !clicked && componentClicked === null, 'hidden': clicked && componentClicked !== componentAnim }]">
-    <div class="flex flex-col space-y-4 lg:space-y-0 lg:flex-row-reverse items-center">
+  <li>
+    <div class="flex flex-col space-y-4 lg:space-y-0 lg:flex-row-reverse items-center h-full">
       <div :class="componentAnim">
         <Link to="#"
           @click.native="titleClicked(componentAnim)" @mouseleave.native="mouseLeave()" @mouseover.native="mouseHover()"
             class="text-white text-display-4 sm:text-display-3 xl:text-display-2 2xl:text-display-1 title relative z-10 title-shadow text-center lg:text-right">
-          {{ title }}
+          <span>{{ title }}</span>
         </Link>
       </div>
       <hr id="line" class="relative border-none h-0.5 rounded-lg bg-white text-white w-4 ml-2 2xl:ml-4 mr-6 xl:mr-8 2xl:mr-12 opacity-0" />
       <p class="text-center lg:text-left text-lg xl:text-xl 2xl:text-2xl font-sans lg:max-w-96 xl:w-max-128 font-normal opacity-0">
         {{ subTitle }}
       </p>
-      <Link to="#" class="hidden" :class="{ 'block z-20 text-white': componentClicked === componentAnim }">
+      <Link to="#" class="hidden">
         {{ buttonMobileText }}
       </Link>
       <component :is="componentAnim" :id="componentAnim" />
@@ -42,50 +42,58 @@ export default defineComponent({
     }
   },
   setup(props) {
-    const hover = ref(false)
-    const clicked = ref(false)
-    let componentClicked = ref(null)
-    const leave = ref(false)
     const titles = ref(['NuxtAnimations', 'DocusAnimations', 'VueTelescopeAnimations'])
 
     //to avoid animation start on load
     onMounted(() => {
-      document.getElementById(props.componentAnim).classList.remove(props.componentAnim)
+      titles.value.forEach(title => {
+        if (document.getElementsByClassName(title)[0] && document.getElementsByClassName(title)[0].classList.contains(title)) {
+          document.getElementById(title).classList.add('hidden')
+        }
+      })
     })
 
-    //add css classes first time on hover
     function mouseHover() {
-      if (!document.getElementById(props.componentAnim).classList.contains(props.componentAnim)) {
-        document.getElementById(props.componentAnim).classList.add(props.componentAnim)
+      if (window.innerWidth > 1024) {
+        loadAnimations()
       }
-      hover.value = true
     }
 
     function mouseLeave() {
-      leave.value = true
-      clicked.value = false
-      hover.value = false
-      componentClicked.value = null
+      titles.value.forEach(title => {
+          document.getElementById(title).classList.add('hidden')
+          document.getElementById(title).parentElement.classList.remove('hidden')
+          document.getElementById(title).parentElement.classList.remove('block')
+        }
+      )
     }
 
-    function titleClicked(component) {
-      clicked.value = true
-      componentClicked.value = component
+    function titleClicked(component: string) {
 
-      if (props.componentAnim !== props.componentAnim) {
-        document.getElementById(props.componentAnim).classList.remove(props.componentAnim)
+      if (window.innerWidth < 1024) {
+        //hide other title on click
+        titles.value.forEach(title => {
+          if (title === component) {
+            document.getElementById(title).parentElement.classList.add('block')
+          } else {
+            document.getElementById(title).parentElement.classList.add('hidden')
+          }
+        })
       }
+
+      loadAnimations()
+    }
+
+    function loadAnimations() {
+      document.getElementById(props.componentAnim).classList.remove('hidden')
+      document.getElementById(props.componentAnim).classList.add('block')
     }
 
     return {
       titles,
-      hover,
       mouseHover,
       mouseLeave,
-      componentClicked,
       titleClicked,
-      clicked,
-      leave
     }
   }
 })
