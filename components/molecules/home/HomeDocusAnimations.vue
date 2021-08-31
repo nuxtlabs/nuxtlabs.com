4a6c9e4ade5f07dd33c849b4c098329835d69834
Florent <flo@nuxtjs.com>
2021-08-31 09:15:42 +0200

    :dizzy: (Home animations) renaming file

---
+++ components/molecules/home/HomeDocusAnimations.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <Ghost class="absolute bottom-30 -left-full lg:right-32 lg:left-auto lg:-bottom-full transform h-40 w-40 z-20 lg:w-72 lg:h-72" />
+    <img id="shape" :src="`img/docus/shape.svg`" class="absolute w-60 lg:w-80 right-0 top-0 lg:bottom-38 lg:h-4/5 lg:-left-10 opacity-0 transform" />
+  </div>
+</template>
+<style lang="postcss" scoped>
+.DocusAnimations {
+  &:hover {
+    ~ div {
+      > #shape {
+        animation: fadeInShapeMobileIn 1s forwards;
+      }
+      @screen lg {
+        > #shape {
+          animation: fadeInShapeDesktopIn 1s forwards;
+        }
+      }
+      .ghost {
+        animation: ghostAnimMobileIn 1s forwards cubic-bezier(0.83, 0, 0.17, 1), ghostFloatX 15s infinite alternate linear;
+      }
+      @screen lg {
+        .ghost {
+          animation: ghostAnimDesktopIn 1s forwards cubic-bezier(0.83, 0, 0.17, 1), ghostFloatX 15s infinite alternate linear;
+        }
+      }
+    }
+  }
+  ~ div {
+    > #shape {
+      animation: fadeInShapeMobileOut 0.5s forwards;
+    }
+    @screen lg {
+      > #shape {
+        animation: fadeInShapeDesktopOut 0.5s forwards;
+      }
+    }
+    .ghost {
+      animation: ghostAnimMobileOut 1s forwards cubic-bezier(0.83, 0, 0.17, 1);
+    }
+    @screen lg {
+      .ghost {
+        animation: ghostAnimDesktopOut 1s forwards cubic-bezier(0.83, 0, 0.17, 1);
+      }
+    }
+  }
+}
+
+@keyframes fadeInShapeDesktopIn {
+  from { opacity: 0; }
+  to { opacity: 1; }
+}
+@keyframes fadeInShapeMobileIn {
+  from { opacity: 0; }
+  to { opacity: 20%; }
+}
+@keyframes fadeInShapeDesktopOut {
+  from { opacity: 1; }
+  to { opacity: 0; }
+}
+@keyframes fadeInShapeMobileOut {
+  from { opacity: 20%; }
+  to { opacity: 0; }
+}
+
+@-webkit-keyframes ghostAnimMobileIn {
+  0% {
+    @apply -left-full;
+  }
+  100% {
+    @apply -left-10 z-20;
+  }
+}
+@-webkit-keyframes ghostAnimDesktopIn {
+  0% {
+    @apply -bottom-full;
+  }
+  100% {
+    @apply -bottom-20 z-20;
+  }
+}
+@-webkit-keyframes ghostAnimMobileOut {
+  0% {
+    @apply left-10 z-20;
+  }
+  100% {
+    @apply -left-full;
+  }
+}
+@-webkit-keyframes ghostAnimDesktopOut {
+  0% {
+    @apply -bottom-20 z-20;
+  }
+  100% {
+    @apply -bottom-full;
+  }
+}
+@-webkit-keyframes ghostFloatX {
+  0% {
+    transform: translateX(0);
+  }
+  50% {
+    transform: translateX(10%);
+  }
+  100% {
+    transform: translateX(-10%);
+  }
+}
+
+</style>
---
+++ components/molecules/home/HomeVueTelescopeAnimations.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <BarLeft class="absolute transform -left-30 -top-10 lg:top-0 bottom-0 opacity-0 stroke-20 sm:stroke-25 md:stroke-30 lg:stroke-50 h-full w-full" />
+    <BarMid class="absolute transform left-1/5 lg:left-1/4 -top-75 lg:-top-10 bottom-0 opacity-0 overflow-visible h-full w-full stroke-25 lg:stroke-32" />
+    <BarBottom class="absolute transform opacity-0 -bottom-100 left-1/2 lg:right-0 h-full w-full stroke-25 lg:stroke-35 xl:stroke-70"/>
+    <img id="vue-telescope" :src="`img/vuetelescope/vue-telescope.svg`" class="absolute transform h-2/5 lg:h-3/5 w-full -left-full bottom-20 lg:-bottom-full lg:-left-1/5" />
+  </div>
+</template>
+<style lang="postcss" scoped>
+.VueTelescopeAnimations {
+  &:hover {
+    ~ div {
+      > #vue-telescope {
+        animation: vueTelescopeAnimMobileIn 600ms forwards cubic-bezier(0.83, 0, 0.17, 1);
+      }
+      @screen lg {
+        > #vue-telescope {
+          animation: vueTelescopeAnimDesktopIn 600ms forwards cubic-bezier(0.83, 0, 0.17, 1);
+        }
+      }
+      .barLeft, .barMid, .barBottom {
+        opacity: 1;
+        stroke-dasharray: 1000;
+        stroke-dashoffset: 1000;
+        animation: dashBarIn 600ms forwards cubic-bezier(0.83, 0, 0.17, 1);
+        z-index: 2;
+      }
+    }
+  }
+  ~ div {
+     > #vue-telescope {
+        animation:vueTelescopeAnimMobileOut 300ms forwards cubic-bezier(0.83, 0, 0.17, 1);
+      }
+      @screen lg {
+        > #vue-telescope {
+          animation:vueTelescopeAnimDesktopOut 300ms forwards cubic-bezier(0.83, 0, 0.17, 1);
+        }
+      }
+    .barLeft, .barMid, .barBottom {
+      opacity: 1;
+      stroke-dasharray: 1000;
+      stroke-dashoffset: 1000;
+      animation: dashBarOut 300ms forwards cubic-bezier(0.83, 0, 0.17, 1);
+      z-index: 2;
+    }
+  }
+}
+
+@keyframes dashBarIn {
+  from {
+    stroke-dashoffset: 900;
+  }
+  to {
+    stroke-dashoffset: 0;
+  }
+}
+@keyframes dashBarOut {
+  from {
+    stroke-dashoffset: 0;
+  }
+  to {
+    stroke-dashoffset: 900;
+  }
+}
+@-webkit-keyframes vueTelescopeAnimDesktopIn {
+  0% {
+    @apply -bottom-full;
+  }
+  100% {
+    @apply -bottom-30 lg:-bottom-40;
+  }
+}
+@-webkit-keyframes vueTelescopeAnimDesktopOut {
+  0% {
+    @apply -bottom-30 lg:-bottom-40;
+  }
+  100% {
+    @apply -bottom-full;
+  }
+}
+@-webkit-keyframes vueTelescopeAnimMobileIn {
+  0% {
+    @apply -left-full;
+  }
+  100% {
+    @apply -left-50 bottom-17 ;
+  }
+}
+@-webkit-keyframes vueTelescopeAnimMobileOut {
+  0% {
+    @apply -left-50 bottom-17;
+  }
+  100% {
+    @apply -left-full;
+  }
+}
+
+</style>
