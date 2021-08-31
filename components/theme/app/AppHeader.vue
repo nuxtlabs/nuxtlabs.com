3a87b6efbc83c9be51d649f55176df997da00045
Florent <flo@nuxtjs.com>
2021-08-31 16:27:17 +0200

    :recycle: (AppHeader) update spacing and links visibility

--- components/theme/app/AppHeader.vue
+++ components/theme/app/AppHeader.vue
@@ -1,25 +1,41 @@
 <template>
-  <header class="absolute w-full top-0 z-50">
-    <div class="d-header xl:p-4 2xl:p-8">
-      <div class="flex flex-row-reverse lg:flex-row h-full flex-none mx-auto px-1 sm:px-3 lg:px-6">
-        <NavigationButton v-if="aside" class="px-2.5" />
-        <Link
-          class="flex items-center flex-1 lg:flex-none lg:w-60"
-          :to="localePath('/')"
-        >
-          <!-- "mr-4 lg:mr-0" to optically center logo text -->
-          <Logo :setings="settings" logoPath="/img/Logo.png"
-            class="hidden lg:block h-5 w-4/5 sm:h-6 md:h-7 mr-4 lg:mr-0" />
-            <!-- Mobile -->
-          <Logo :setings="settings" logoPath="icon.png"
-            class="lg:hidden h-10 w-14 sm:h-14 sm:w-18 md:h-20 md:w-24 lg:w-4/5 lg:h-8" />
-        </Link>
+  <header class="d-header">
+    <nav class="flex h-full flex-none text-gray-400 mx-auto px-2 sm:px-3 lg:px-6">
+      <Link
+        class="flex items-center flex-1"
+        :to="localePath('/')"
+      >
+        <!-- "mr-4 lg:mr-0" to optically center logo text -->
+        <Logo :setings="settings" logoPath="/img/logo.svg"
+          class="h-6 lg:h-8 mb-2 mr-4 lg:mr-0" />
+        <!-- Mobile -->
+        <!-- Logo :setings="settings" logoPath="icon.png"
+          class="lg:hidden h-10 w-14 sm:h-14 sm:w-18 md:h-20 md:w-24 lg:w-4/5 lg:h-8" / -->
+      </Link>
 
-        <div class="items-center hidden lg:flex lg:flex-1">
-          <HeaderNavigation />
-        </div>
+      <div v-if="!isHome" class="justify-center hidden lg:flex">
+        <Link
+          v-for="{ title, href } in articleLinks"
+          :key="title"
+          :aria-label="title"
+          class="relative flex items-center justify-center h-full px-4 font-medium text-center lg:text-lg capitalize group hover:text-white"
+          :to="href"
+        >
+        {{ title }}
+        </Link>
       </div>
-    </div>
+      <div class="flex items-center justify-end flex-1">
+        <Link
+          v-for="{ title, href } in links"
+          :key="title"
+          :aria-label="title"
+          class="relative flex items-center h-full font-medium text-center lg:text-lg capitalize group hover:text-white"
+          :to="href"
+        >
+        {{ title }}
+        </Link>
+      </div>
+    </nav>
   </header>
 </template>
 
@@ -28,16 +44,50 @@ import { computed, defineComponent, useContext } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   props: {
-    aside: {
+    projects: {
       type: Boolean,
       default: false
+    },
+    links: {
+      type: Array,
+      required: false,
+      default: () => [
+        {
+          title: 'About us',
+          href: '/about',
+        }
+      ]
+    },
+    articleLinks: {
+      type: Array,
+      required: false,
+      default: () => [
+        {
+          title: 'NuxtJS',
+          href: '/nuxtjs'
+        },
+        {
+          title: 'Docus',
+          href: '/docus'
+        },
+        {
+          title: 'Vue telescope',
+          href: '/vuetelescope'
+        }
+      ]
     }
   },
   setup() {
-    const { $docus } = useContext()
+    const { $docus, route } = useContext()
     const settings = computed(() => $docus.settings.value)
+    const isHome = computed(() => route.value.path === '/')
 
-    return { settings }
+    return { settings, isHome }
   }
 })
 </script>
+<style lang="postcss" scoped>
+.nuxt-link-active {
+  color: white
+}
+</style>
