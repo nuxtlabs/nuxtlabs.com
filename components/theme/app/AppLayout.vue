0ea9306a1080451ad704734ed47cc1678f1c3d5d
Florent <flo@nuxtjs.com>
2021-08-31 16:28:55 +0200

    :recycle: (AppLayout) refacto classes

--- components/theme/app/AppLayout.vue
+++ components/theme/app/AppLayout.vue
@@ -1,23 +1,7 @@
 <template>
-  <div class="w-full h-screen">
-    <div class="cursor cursor--small"></div>
-    <canvas class="cursor cursor--canvas" resize></canvas>
-    <slot v-if="layout.header" name="header">
-      <AppHeader :aside="layout.aside" />
-    </slot>
-
-    <div class="flex h-full" :class="layout.fluid ? '' : 'd-container'">
-      <slot v-if="layout.aside" name="aside">
-        <AppAside :class="layout.asideClass" />
-      </slot>
-
-      <!-- TODO: possibility to unwrap div ? -->
-      <div class="flex-auto w-full min-w-0 lg:static lg:max-h-screen mt-8 fix-wrapper">
-        <slot />
-      </div>
-    </div>
-
-    <slot v-if="layout.footer" name="footer"><AppFooter /></slot>
+  <div class="relative w-full">
+    <AppHeader :projects="layout.projects" />
+    <slot />
   </div>
 </template>
 
@@ -36,30 +20,3 @@ export default defineComponent({
   }
 })
 </script>
-<style lang="postcss">
-.fix-wrapper > div {
-  @apply h-full;
-  padding-top: calc(var(--header-height));
-}
-
-.cursor {
-  position: fixed;
-  left: 0;
-  top: 0;
-  pointer-events: none;
-}
-.cursor--small {
-  width: 5px;
-  height: 5px;
-  left: -2.5px;
-  top: -2.5px;
-  border-radius: 50%;
-  z-index: 11000;
-  background: var(--color-text);
-}
-.cursor--canvas {
-  width: 100vw;
-  height: 100vh;
-  z-index: 12000;
-}
-</style>
