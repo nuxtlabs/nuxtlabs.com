2794c508c2d101e501a1a4ecd7c724339c57f2d4
Florent <flo@nuxtjs.com>
2021-09-01 15:55:55 +0200

    :wrench: (nuxt.config) update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,35 +12,15 @@ export default withDocus({
         href: 'https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap'
       },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-      { rel: 'icon', type: 'image/x-icon', href: '/icon.png' }
+      { rel: 'icon', type: 'image/png', href: '/icon.png' }
     ],
     meta: [
       { hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt.js' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       {
-        hid: 'twitter:card',
-        name: 'twitter:card',
-        content: 'summary_large_image'
-      },
-      {
-        hid: 'og:image',
-        property: 'og:image',
-        content: 'https://nuxtjs.org/preview.png'
-      },
-      {
-        hid: 'og:image:secure_url',
-        property: 'og:image:secure_url',
-        content: 'https://nuxtjs.org/preview.png'
-      },
-      {
         hid: 'og:image:alt',
         property: 'og:image:alt',
         content: 'Nuxt.js'
-      },
-      {
-        hid: 'twitter:image',
-        name: 'twitter:image',
-        content: 'https://nuxtjs.org/preview.png'
       }
     ],
     bodyAttrs: {
