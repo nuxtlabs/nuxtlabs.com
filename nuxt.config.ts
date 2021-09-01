6eac1a4380d6c8655bc4633f4f9114d8411f3c06
Florent <flo@nuxtjs.com>
2021-09-01 11:44:04 +0200

    :wrench: (nuxt.config) add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,8 @@ export default withDocus({
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap'
       },
-      { rel: 'preconnect', href: 'https://fonts.gstatic.com' }
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'icon', type: 'image/x-icon', href: '/icon.png' }
     ],
     meta: [
       { hid: 'og:site_name', property: 'og:site_name', content: 'Nuxt.js' },
