2204718b5cd3268bd6d5ab14798e25d819f23cc5
Florent <flo@nuxtjs.com>
2021-09-01 12:27:16 +0200

    :wheelchair:  add missing alt on images

--- components/molecules/SocialLinks.vue
+++ components/molecules/SocialLinks.vue
@@ -3,7 +3,7 @@
     <li v-for="link in links" :key="link.name">
       <Link :to="link.href" :alt="link.name" blank class="relative z-50 gradient">{{ link.name }}</Link>
       <div class="hidden lg:block absolute opacity-60 w-128 h-128 z-0 pointer-events-none -bottom-full -left-full">
-        <img :src="`/img/socialGradients/${link.gradientImg}.svg`" class="relative" />
+        <img :src="`/img/socialGradients/${link.gradientImg}.svg`" alt="Gradient effect" class="relative" />
       </div>
     </li>
   </ul>
