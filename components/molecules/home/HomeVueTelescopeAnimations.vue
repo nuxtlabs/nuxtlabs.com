ce8f8769d811cbcd4b53f5ca66c54b90b404bc35
Florent <flo@nuxtjs.com>
2021-09-01 16:31:45 +0200

    :bug: (VueTelescopeAnimations) fix image spacing

--- components/molecules/home/HomeVueTelescopeAnimations.vue
+++ components/molecules/home/HomeVueTelescopeAnimations.vue
@@ -3,7 +3,7 @@
     <BarLeft class="absolute transform hidden lg:block lg:-left-60 xl:-left-80 -top-10 lg:top-0 bottom-0 opacity-0 stroke-20 sm:stroke-25 md:stroke-25 lg:stroke-32 h-full w-full" />
     <BarMid class="absolute transform left-1/5 lg:left-1/12 xl:left-1/5 -top-75 lg:-top-25 bottom-0 opacity-0 overflow-visible h-full w-full stroke-25 lg:stroke-32" />
     <BarBottom class="absolute transform opacity-0 -bottom-100 left-1/2 lg:right-0 h-full w-full stroke-25 lg:stroke-32"/>
-    <img id="vue-telescope" :src="`img/vuetelescope/vue-telescope.svg`" alt="Vue Telescope logo" class="absolute transform h-2/5 lg:h-3/6 w-full -left-full top-20 lg:-bottom-full lg:-left-1/6" />
+    <img id="vue-telescope" :src="`img/vuetelescope/vue-telescope.svg`" alt="Vue Telescope logo" class="absolute transform h-2/5 lg:h-3/6 w-full -left-full bottom-50 lg:-bottom-full lg:-left-1/6" />
   </div>
 </template>
 <style lang="postcss" scoped>
@@ -80,18 +80,18 @@
 }
 @-webkit-keyframes vueTelescopeAnimMobileIn {
   0% {
-    @apply -left-full;
+    @apply -left-full opacity-0;
   }
   100% {
-    @apply -left-90 top-20 ;
+    @apply -left-50 bottom-17 opacity-30;
   }
 }
 @-webkit-keyframes vueTelescopeAnimMobileOut {
   0% {
-    @apply -left-90 top-20;
+    @apply -left-50 bottom-17 opacity-30;
   }
   100% {
-    @apply -left-full;
+    @apply -left-full opacity-0;
   }
 }
 
