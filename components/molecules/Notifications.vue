dd0cd02de06fab33697b262334c3d6ad2918da06
Florent <flo@nuxtjs.com>
2021-12-16 14:59:17 +0100

    fix(Notifications): update

--- components/molecules/Notifications.vue
+++ components/molecules/Notifications.vue
@@ -3,12 +3,12 @@
     class="fixed bottom-0 right-0 flex flex-col justify-end w-full z-50 sm:w-96"
   >
     <div
-      v-if="$notifications.state.value.length"
+      v-if="notifications.length"
       class="px-4 py-6 space-y-3 overflow-y-auto sm:px-6 lg:px-8"
     >
       <div
-        v-for="(notification, index) of $notifications.state.value"
-        v-show="index === $notifications.state.value.length - 1"
+        v-for="(notification, index) of notifications"
+        v-show="index === notifications.length - 1"
         :key="notification.id"
       >
         <Notification
@@ -21,3 +21,19 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import { useNotifications } from '~/plugins/notifications'
+
+export default defineComponent({
+  setup() {
+    const { notifications, remove } = useNotifications()
+
+    return {
+      remove,
+      notifications,
+    }
+  },
+})
+</script>
