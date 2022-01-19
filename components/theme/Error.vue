d28fcd2cec8c4ff48300aa3ba2123343d35abfc8
Florent <flo@nuxtjs.com>
2022-01-19 11:18:38 +0100

    fix(Error): remove links and logo

--- components/theme/Error.vue
+++ components/theme/Error.vue
@@ -2,14 +2,9 @@
   <div
     class="py-8 mt-12 xs:mt-14 sm:mt-16 md:mt-18 lg:mt-24 sm:py-16 max-w-6xl mx-auto px-4 sm:px-8 lg:px-20"
   >
-    <main
+    <div
       class="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8"
     >
-      <div class="flex-shrink-0 flex justify-center">
-        <a href="/" class="inline-flex">
-          <nuxt-img src="/img/logo.svg" class="h-full w-auto" alt="NuxtLabs" />
-        </a>
-      </div>
       <div class="py-16">
         <div class="text-center">
           <p
@@ -37,33 +32,6 @@
           </div>
         </div>
       </div>
-    </main>
-    <footer class="flex-shrink-0 max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
-      <nav class="flex justify-center space-x-4">
-        <a
-          href="#"
-          class="text-sm font-medium text-gray-500 hover:text-gray-600"
-          >Contact Support</a
-        >
-        <span
-          class="inline-block border-l border-gray-300"
-          aria-hidden="true"
-        ></span>
-        <a
-          href="#"
-          class="text-sm font-medium text-gray-500 hover:text-gray-600"
-          >Status</a
-        >
-        <span
-          class="inline-block border-l border-gray-300"
-          aria-hidden="true"
-        ></span>
-        <a
-          href="#"
-          class="text-sm font-medium text-gray-500 hover:text-gray-600"
-          >Twitter</a
-        >
-      </nav>
-    </footer>
+    </div>
   </div>
 </template>
